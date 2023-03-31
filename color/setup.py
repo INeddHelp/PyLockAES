6747a55cb6262ebd447ed76da1c8a106464cbe58
INeedHelp <leonardo.bartolelli@gmail.com>
2023-03-31 21:55:20 +0200

    PyPuts v1.0

--- color/setup.py
+++ color/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='ChromaticPrint',
+    name='PyPuts',
     version='1.0',
-    description='A library for printing colored text in the terminal',
+    description='PyPuts: A Python Library for Colorful and Readable Console Output',
     author='INeddHelp',
     author_email='ineddhelpgithub@gmail.com',
-    url='https://github.com/INeddHelp/Colors-python-library',
-    packages=['chromaticprint']
+    url='https://github.com/INeddHelp/PyPuts',
+    packages=['pyputs']
 )
