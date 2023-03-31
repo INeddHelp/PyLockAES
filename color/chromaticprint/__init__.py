3c381b80b900f21dbef89518490282dea427252b
INeedHelp <leonardo.bartolelli@gmail.com>
2023-03-31 20:29:23 +0200

    v1.0

---
+++ color/chromaticprint/__init__.py
@@ -0,0 +1,44 @@
+escape_codes = {
+    "reset": "\033[0m",
+    "bold": "\033[1m",
+    "dim": "\033[2m",
+    "underline": "\033[4m",
+    "blink": "\033[5m",
+    "reverse": "\033[7m",
+    "hidden": "\033[8m",
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "magenta": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m",
+    "bg_black": "\033[40m",
+    "bg_red": "\033[41m",
+    "bg_green": "\033[42m",
+    "bg_yellow": "\033[43m",
+    "bg_blue": "\033[44m",
+    "bg_magenta": "\033[45m",
+    "bg_cyan": "\033[46m",
+    "bg_white": "\033[47m",
+}
+
+
+class Colors:
+    def __init__(self, escape_codes):
+        self.escape_codes = escape_codes
+
+    def __getattr__(self, key):
+        if key in self.escape_codes:
+            return self.escape_codes[key]
+        else:
+            raise AttributeError(f"'Colors' object has no attribute '{key}'")
+
+
+colors = Colors(escape_codes)
+
+
+def puts(the_color_chosen, text):
+    print(f"{getattr(colors, the_color_chosen)}{text}{colors.reset}")
+    
---
+++ color/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ChromaticPrint',
+    version='1.0',
+    description='A library for printing colored text in the terminal',
+    author='INeddHelp',
+    author_email='ineddhelpgithub@gmail.com',
+    url='https://github.com/INeddHelp/Colors-python-library',
+    packages=['chromaticprint']
+)
