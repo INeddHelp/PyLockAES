df4078a6abca3a6e6abeb9b9d4c7b88147ddf698
Michael Barati <101454502+INeddHelp@users.noreply.github.com>
2023-04-01 22:24:18 +0200

    Delete test directory

--- test/example.py
+++
@@ -1,9 +0,0 @@
-from pylockaes import AESEncryption
-
-encryption = AESEncryption("password")
-
-# Encrypt a file
-encryption.encrypt_file("text.txt", "encrypted.txt")
-
-# Decrypt a file
-encryption.decrypt_file("encrypted.txt", "decrypted.txt")
